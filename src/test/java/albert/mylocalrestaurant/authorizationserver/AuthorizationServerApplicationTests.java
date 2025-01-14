45d364806e6fe9a77cc22ec0f3952ab28e3f02e2
ALbertIM0427 <guang7226961@naver.com>
2025-01-14 16:12:15 +0900

    chore: 프로젝트 초기화

---
+++ src/test/java/albert/mylocalrestaurant/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,15 @@
+package albert.mylocalrestaurant.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/albert/mylocalrestaurant/authorizationserver/TestAuthorizationServerApplication.java
@@ -0,0 +1,11 @@
+package albert.mylocalrestaurant.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(AuthorizationServerApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
